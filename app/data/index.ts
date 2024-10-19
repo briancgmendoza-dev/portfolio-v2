987948f3299fc63e2623c53784147f9c68bbc1d2
Brian Christopher G. Mendoza <briancgmendoza@gmail.com>
2024-10-19 21:12:46 +0800

    [Data] Updating data

---
+++ app/data/index.ts
@@ -0,0 +1,27 @@
+import { profile } from "@/app/data/profile.json"
+import { backend } from "@/app/data/backend.json"
+import { links } from "@/app/data/links.json"
+import { languages } from "@/app/data/languages.json"
+import { frontend } from "@/app/data/frontend.json"
+import { databases } from "@/app/data/databases.json"
+import { practices } from "@/app/data/practices.json"
+import { cloud } from "@/app/data/cloud.json"
+import { tools } from "@/app/data/tools.json"
+import { experience } from "@/app/data/work-experience.json"
+import { reference } from "@/app/data/character-reference.json"
+import { education } from "@/app/data/education.json"
+
+export {
+  profile,
+  links,
+  languages,
+  frontend,
+  backend,
+  databases,
+  practices,
+  cloud,
+  tools,
+  experience,
+  reference,
+  education
+}
