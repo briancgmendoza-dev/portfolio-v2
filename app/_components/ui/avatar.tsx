43b0b78e22c2d5a549db2d7453fbfcbbdef7006f
Brian Christopher G. Mendoza <briancgmendoza@gmail.com>
2024-10-19 21:14:55 +0800

    [UI] Implementing avatar boilerplate

---
+++ app/_components/ui/avatar.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Avatar() {
+  return (
+    <Image src="" alt="Profile Picture" className='mx-auto w-20 h-20 rounded-full border' />
+  )
+}
