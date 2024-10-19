e5c29b3d539327690714bc65f0e6307e47ba36c6
Brian Christopher G. Mendoza <briancgmendoza@gmail.com>
2024-10-19 21:14:14 +0800

    [Sections] Implementing profile section boilerplate

---
+++ app/_components/sections/profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Avatar from '@/app/_components/ui/avatar'
+import { profile, links } from '@/app/data'
+
+export default function Profile() {
+  return (
+    <section className='text-white pt-10'>
+      <Avatar />
+      <ul>
+        {Object.entries(profile).map(([key, value]) => (
+          <li key={key}>
+            {value ? value : 'N/A'}
+          </li>
+        ))}
+      </ul>
+      <ul className='flex items-center justify-center'>
+        {links.map((link, index) => (
+          <li
+            key={index}
+          >
+            {link.name}
+            {link.icon ? (
+              <img src={link.icon} alt={link.name} />
+            ) : (
+              <p>No Icon</p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
