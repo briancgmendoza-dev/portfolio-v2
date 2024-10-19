da2612e27f9a713c064feb1059c52ba8d9513ccb
Brian Christopher G. Mendoza <briancgmendoza@gmail.com>
2024-10-19 21:14:36 +0800

    [Page] Implementing page boilerplate

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Profile from "@/app/_components/sections/profile";
+import Skills from "@/app/_components/sections/skills";
+import Experience from "@/app/_components/sections/experience";
+import Projects from "@/app/_components/sections/projects";
+import Education from "@/app/_components/sections/education";
+
 export default function Home() {
   return (
-    <div className="items-center justify-items-center min-h-screen">
-      Hello
-    </div>
+    <main className="items-center justify-items-center min-h-screen bg-black">
+      <Profile />
+      <Skills />
+      <Experience />
+      <Projects />
+      <Education />
+    </main>
   );
 }
